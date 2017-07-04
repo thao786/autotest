7a4b3a5f7e4d2d1b77acf8c8b6966df5122e935b
fall <xiaoyuner2014@gmail.com>
2017-07-04 14:50:00 -0400

    view more done

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -1,4 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$( ".slice-me" ).slice( 0, 50 ).hide();
+
+    function showmore(id) {
+        if ($("#vmbutton").text() == "View Less") {
+            $("#vmbutton").html('View More');
+            $("#" + id).css("display", "block");
+        }
+        else {
+            $("#vmbutton").html('View Less');
+            $("#" + id).css("display", "none");
+        }
+
+    }
