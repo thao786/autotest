e81b98aad1cfe2f2482c55da94bfadc672150f83
Thao Nguyen <thao786@Thaos-MacBook-Pro.local>
2017-07-07 13:26:58 -0400

    get user detail from google

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,10 @@ class User < ApplicationRecord
     unless user
         user = User.create(name: data['name'],
            email: data['email'],
-           password: Devise.friendly_token[0,20]
+           password: Devise.friendly_token[0,20],
+           provider: access_token.provider,
+           uid: access_token.uid,
+           image: data['image']
         )
     end
     user
