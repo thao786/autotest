056c689f946a890169d15cf0d29c475496d7bedb
Thao Nguyen <thao786@Thaos-MacBook-Pro.local>
2017-07-07 20:44:01 -0400

    all works

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,4 +266,10 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
+  config.omniauth :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'],
+          {
+              scope: 'userinfo.email,userinfo.profile',
+              provider_ignores_state: true
+          }
+
 end
--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'],
-           { scope: 'userinfo.email,userinfo.profile',
-             provider_ignores_state: true
-           }
-end
