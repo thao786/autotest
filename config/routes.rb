99fc8e9c345e97de08596a2d50aee80815ce3fe8
Thao Nguyen <thao786@Thaos-MacBook-Pro.local>
2017-07-16 18:09:36 -0400

    relink doc page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'dashboard/index'
   get 'dashboard/intro'
   get 'application/documentation'
-  get '/documentation', to: 'application#documentation'
+  get '/doc', to: 'application#documentation'
 
   get 'fonts/*font', to: 'fonts#index'
 
