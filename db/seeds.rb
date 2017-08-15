caf197fd64fd34cb1589107affe60ee081f8db79
Thao Nguyen <thao786@Thaos-MacBook-Pro.local>
2017-08-15 18:54:03 -0400

    fix doc

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-Assertion.create(id: 1, assertion_type: "http-200", label: "All Http request return 200 status")
-Assertion.create(id: 2, assertion_type: "ajax-200", label: "ajax 200")
-Assertion.create(id: 3, assertion_type: "report", label: "report")
-Assertion.create(id: 4, assertion_type: "step-succeed", label: "step completes successfully")
-Assertion.create(id: 5, assertion_type: "match-url", label: "url must match")
+Assertion.create(id: 1, assertion_type: "http-200")
+Assertion.create(id: 2, assertion_type: "ajax-200")
+Assertion.create(id: 3, assertion_type: "report")
+Assertion.create(id: 4, assertion_type: "step-succeed")
+Assertion.create(id: 5, assertion_type: "match-url")
 
 
 ActiveRecord::Base.connection.execute("ALTER TABLE results CHANGE created_at created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP")
