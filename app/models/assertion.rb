67875ac49a5aaa3adbaebe469eac245b5990dc3e
Thao Nguyen <thao786@Thaos-MacBook-Pro.local>
2017-08-15 21:06:53 -0400

    try stackoverflow slt

--- app/models/assertion.rb
+++ app/models/assertion.rb
@@ -9,13 +9,15 @@ class Assertion < ApplicationRecord
   end
 
   def self.assertion_types
-    {'text-in-page' => 'Text Contained in Page',
-     'text-not-in-page' => 'Text Not Contained in Page',
-     'html-in-page' => 'HTML Contained in Page',
-     'html-not-in-page' => 'HTML not Contained in Page',
-     'page-title' => 'Page Title',
-     # 'status-code' => 'Status Code',
-     'self-enter' => 'Enter Your Own Condition'}
+    {
+        'text-in-page' => 'Text Contained in Page',
+         'text-not-in-page' => 'Text Not Contained in Page',
+         'html-in-page' => 'HTML Contained in Page',
+         'html-not-in-page' => 'HTML not Contained in Page',
+         'page-title' => 'Page Title',
+         # 'status-code' => 'Status Code',
+         'self-enter' => 'Enter Your Own Condition'
+    }
   end
 
   def self.default_assertions
