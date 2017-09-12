5a25f6a84300460331d2f3ca25262476025f8ab2
Thao Nguyen <thao786@Thaos-MacBook-Pro.local>
2017-09-12 15:00:46 -0400

    complete

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -115,12 +115,24 @@ module TestsHelper
     FileUtils.rm_r folder if Dir.exist?(folder)
     Dir.mkdir folder
 
-    unless Rails.env.development?
+    if Rails.env.development?
+      driver = Selenium::WebDriver.for :chrome
+    else
       headless = Headless.new
       headless.start
+
+      caps = Selenium::WebDriver::Remote::Capabilities.chrome('desiredCapabilities' => {'takesScreenshot' => true}, 'chromeOptions' => {'binary' => '/chromium-browser'})
+
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('--headless')
+      options.add_argument('--no-sandbox')
+      options.add_argument('--disable-gpu')
+      options.add_argument('--remote-debugin-port=9222')
+      options.add_argument('--screen-size=1200x800')
+
+      driver = Selenium::WebDriver.for :chrome, desired_capabilities: caps, options: options
     end
 
-    driver = Selenium::WebDriver.for :firefox
     begin
       runSteps(driver, test, test.id)
     rescue
