239d0c09314f85b3d589b0acb7cbf423cfa385c1
Thao Nguyen <thao786@Thaos-MacBook-Pro.local>
2017-09-18 18:39:33 -0400

    add canvas

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -37,81 +37,83 @@ module ResultsHelper
         driver.switch_to.window tab_id
       end
 
-      sleep step.wait/1000 # wait
-
       begin
-        case step.action_type
-          when 'pageload'
-            driver.get extractParams(paramStr,step.webpage)
-          when 'pageloadCurl'
-            # load headers and params
-            Thread.new {
+        Timeout::timeout(step.wait/1000) { # wait
+          case step.action_type
+            when 'pageload'
               driver.get extractParams(paramStr,step.webpage)
-            }
-          when 'scroll'
-            driver.execute_script("scroll(#{extractParams(paramStr,step.scrollLeft)}, #{extractParams(paramStr,step.scrollTop)})")
-          when 'keypress'
-            driver.action.send_keys(extractParams(paramStr,step.typed)).perform
-          when 'click'
-            type = extractParams(paramStr,step.selector[:selectorType])
-            selector = extractParams(paramStr,step.selector[:selector])
-            eq = extractParams(paramStr,step.selector[:eq])
-            element = case type # first, find DOM with WebDriver
-                        when 'id'
-                          driver.find_elements(:id => selector).first
-                        when 'class'
-                          driver.find_elements(:class => selector)[eq]
-                        when 'tag'
-                          driver.find_elements(:tag_name => selector)[eq]
-                        when 'name'
-                          driver.find_elements(:name => selector)[eq]
-                        when 'partialLink' # link text
-                          driver.find_elements(:partial_link_text => selector)[eq]
-                        when 'href'
-                          driver.find_elements(:css => "a[href='#{selector}']")[eq]
-                        when 'partialHref'
-                          driver.find_elements(:css => "a[href*='#{selector}']")[eq]
-                        when 'button' # use XPath
-                          driver.find_elements(:xpath, "//button[text()[contains(.,'#{selector}')]]")[eq]
-                        when 'css'
-                          driver.find_elements(:css => selector)[eq]
-                        when 'coordination'
-                          elem = driver.find_elements(:tag_name => 'body').first
-                          driver.action.move_to(elem, 50, 50).click.perform
-                          elem
-                        else
-                          nil
-                    end
-            if element.present?
-              element.click
-            else # when WebDriver's find_elements fails, find with JS
-              js_selector = case type
-                              when 'id'
-                                "document.getElementById('#{selector}')"
-                              when 'class'
-                                "document.getElementsByClassName('#{selector}')[#{eq}]"
-                              when 'tag'
-                                "document.getElementsByTagName('#{selector}')[#{eq}]"
-                              when 'name'
-                                "document.getElementsByName('#{selector}')[#{eq}]"
-                              when 'partialLink' # use XPath, cant use eq
-                                "document.evaluate('//a[text()[contains(.,\'#{selector}')]]\' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue"
-                              when 'href'
-                                "document.querySelectorAll('a[href=\'#{selector}\']')[#{eq}]"
-                              when 'partialHref'
-                                "document.querySelectorAll('a[href*=\'#{selector}\']')[#{eq}]"
-                              when 'button' # use XPath, cant use eq
-                                "document.evaluate('//button[text()[contains(.,\'#{selector}')]]\' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue"
-                              when 'css'
-                                "document.querySelectorAll('#{selector}')[#{eq}]"
-                              else
-                                nil
-                            end
-              driver.execute_script("#{js_selector}.click()")
-            end
-          else
-            true
-        end
+            when 'pageloadCurl'
+              # load headers and params
+              Thread.new {
+                driver.get extractParams(paramStr,step.webpage)
+              }
+            when 'scroll'
+              driver.execute_script("scroll(#{extractParams(paramStr,step.scrollLeft)}, #{extractParams(paramStr,step.scrollTop)})")
+            when 'keypress'
+              driver.action.send_keys(extractParams(paramStr,step.typed)).perform
+            when 'click'
+              type = extractParams(paramStr,step.selector[:selectorType])
+              selector = extractParams(paramStr,step.selector[:selector])
+              eq = extractParams(paramStr,step.selector[:eq])
+              element = case type # first, find DOM with WebDriver
+                          when 'id'
+                            driver.find_elements(:id => selector).first
+                          when 'class'
+                            driver.find_elements(:class => selector)[eq]
+                          when 'tag'
+                            driver.find_elements(:tag_name => selector)[eq]
+                          when 'name'
+                            driver.find_elements(:name => selector)[eq]
+                          when 'partialLink' # link text
+                            driver.find_elements(:partial_link_text => selector)[eq]
+                          when 'href'
+                            driver.find_elements(:css => "a[href='#{selector}']")[eq]
+                          when 'partialHref'
+                            driver.find_elements(:css => "a[href*='#{selector}']")[eq]
+                          when 'button' # use XPath
+                            driver.find_elements(:xpath, "//button[text()[contains(.,'#{selector}')]]")[eq]
+                          when 'css'
+                            driver.find_elements(:css => selector)[eq]
+                          when 'coordination'
+                            elem = driver.find_elements(:tag_name => 'body').first
+                            driver.action.move_to(elem, 50, 50).click.perform
+                            elem
+                          else
+                            nil
+                      end
+              if element.present?
+                element.click
+              else # when WebDriver's find_elements fails, find with JS
+                js_selector = case type
+                                when 'id'
+                                  "document.getElementById('#{selector}')"
+                                when 'class'
+                                  "document.getElementsByClassName('#{selector}')[#{eq}]"
+                                when 'tag'
+                                  "document.getElementsByTagName('#{selector}')[#{eq}]"
+                                when 'name'
+                                  "document.getElementsByName('#{selector}')[#{eq}]"
+                                when 'partialLink' # use XPath, cant use eq
+                                  "document.evaluate('//a[text()[contains(.,\'#{selector}')]]\' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue"
+                                when 'href'
+                                  "document.querySelectorAll('a[href=\'#{selector}\']')[#{eq}]"
+                                when 'partialHref'
+                                  "document.querySelectorAll('a[href*=\'#{selector}\']')[#{eq}]"
+                                when 'button' # use XPath, cant use eq
+                                  "document.evaluate('//button[text()[contains(.,\'#{selector}')]]\' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue"
+                                when 'css'
+                                  "document.querySelectorAll('#{selector}')[#{eq}]"
+                                else
+                                  nil
+                              end
+                driver.execute_script("#{js_selector}.click()")
+              end
+            else
+              true
+          end
+        }
+      rescue Timeout::Error
+        # carry on
       rescue Exception => error
         Result.create(test: test, step: step, webpage: driver.current_url,
                       assertion: Assertion.where(assertion_type: "step-succeed").first,
@@ -136,8 +138,6 @@ module ResultsHelper
       bucket = resource.bucket(ENV['bucket'])
       bucket.object("#{md5}.png").upload_file(screenshot, acl:'public-read')
     }
-
-    sleep 1
 
     if checkAssertions # check assertions
       tabs.each { |tab_id|
