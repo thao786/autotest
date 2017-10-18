4786cfd87403d5bac2700c1bceff7ff2ed3e7f35
Thao Nguyen <roseskindergarten@gmail.com>
2017-10-17 21:53:47 -0400

    detect chrome

--- README.md
+++ README.md
@@ -27,4 +27,6 @@ Thao:
 
 
 
+VIDEO SCRIPT
+
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -131,3 +131,17 @@ window.gpAsyncInit = function() {
         });
     });
 };
+
+
+
+function getBrowser() {
+    if (typeof chrome !== "undefined") {
+        if (typeof browser !== "undefined") {
+            return "Firefox";
+        } else {
+            return "Chrome";
+        }
+    } else {
+        return "Edge";
+    }
+}
