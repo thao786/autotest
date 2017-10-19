f2b5270b09b944d825e4a8496db02b53a0285944
Thao Nguyen <roseskindergarten@gmail.com>
2017-10-19 14:53:36 -0400

    fix sign in redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   include Util
+
+  protected
+  def after_sign_in_path_for(resource)
+    '/suites'
+  end
 end
