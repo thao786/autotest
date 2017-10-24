801e02309b9a39866f8673b831309cfc578991ba
Thao Nguyen <roseskindergarten@gmail.com>
2017-10-24 18:06:01 -0400

    debug

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -10,24 +10,24 @@ class ApiController < ActionController::Base
   # save this event to draft and update test's cache response
   def saveEvent
     begin
-    if params[:action_type] == 'pageload' and params[:webpage] =~ %r|http(.*)google(.*)/_/chrome/newtab|
-      render json: {}, :status => 404
-    else
-      test = Test.find_by(session_id: params[:session_id])
-
-      if test and test.session_expired_at > DateTime.now
-        test.update(session_expired_at: (Time.now + 15*60))
-        hash = params.to_unsafe_h
-        params[:draft] = hash
-
-        draft = Draft.create(params.require(:draft).permit(:webpage, :stamp, :apk, :activity, :action_type, :session_id, :typed, :screenwidth, :screenheight, :scrollTop, :scrollLeft, :x, :y, :tabId, :windowId))
-
-        draft.update(selector: {selector: params[:selector], eq: params[:eq].to_i, selectorType: params[:selectorType], childrenCount: params[:childrenCount].to_i})
-        render json: {}
-      else
+      if params[:action_type] == 'pageload' and params[:webpage] =~ %r|http(.*)google(.*)/_/chrome/newtab|
         render json: {}, :status => 404
+      else
+        test = Test.find_by(session_id: params[:session_id])
+
+        if test and test.session_expired_at > DateTime.now
+          test.update(session_expired_at: (Time.now + 15*60))
+          hash = params.to_unsafe_h
+          params[:draft] = hash
+
+          draft = Draft.create(params.require(:draft).permit(:webpage, :stamp, :apk, :activity, :action_type, :session_id, :typed, :screenwidth, :screenheight, :scrollTop, :scrollLeft, :x, :y, :tabId, :windowId))
+
+          draft.update(selector: {selector: params[:selector], eq: params[:eq].to_i, selectorType: params[:selectorType], childrenCount: params[:childrenCount].to_i})
+          render json: {}
+        else
+          render json: {}, :status => 404
+        end
       end
-    end
     rescue Exception => error
       render json: {:errors => error.message}, :status => 404
     end
