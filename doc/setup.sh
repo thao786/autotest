c77ba359aec9a14174ddb3d0ae257e94bd3e443f
Thao Nguyen <roseskindergarten@gmail.com>
2017-10-27 14:29:47 -0400

    fix plugin

--- doc/setup.sh
+++ doc/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# connect to instance:
-ssh -i "ec2.pem" ubuntu@ec2-34-213-223-51.us-west-2.compute.amazonaws.com
-
-
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
