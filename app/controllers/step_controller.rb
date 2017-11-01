0f5498fadbd1fdb9164c2b5b87cc409a014133f2
Thao Nguyen <roseskindergarten@gmail.com>
2017-11-01 15:33:46 -0400

    update

--- app/controllers/step_controller.rb
+++ app/controllers/step_controller.rb
@@ -1,5 +1,5 @@
 class StepController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   before_action :set_step, :set_test
   before_action :authorize?
